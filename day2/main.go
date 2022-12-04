4891c4b89ec44cd4aeea3fc7257866bcd2aaeb33
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2022-12-04 18:25:20 +0100

    feat: complete day 2 part 2 + rebase a bit

--- day2/main.go
+++ day2/main.go
@@ -19,13 +19,8 @@ func contains(s []string, str string) bool {
 	return false
 }
 
-func main() {
-	f, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	scanner := bufio.NewScanner(f)
+func partOne(file *os.File) int {
+	scanner := bufio.NewScanner(file)
 
 	winningCombinations := []string{"A Y", "B Z", "C X"}
 	drawingCombinations := []string{"A X", "B Y", "C Z"}
@@ -33,9 +28,6 @@ func main() {
 	points := 0
 	for scanner.Scan() {
 		strategy := scanner.Text()
-		if err != nil {
-			log.Fatalln(err)
-		}
 
 		switch yourHand := string(strategy[2]); yourHand {
 		case "X":
@@ -51,9 +43,83 @@ func main() {
 		} else if contains(drawingCombinations, strategy) {
 			points += 3
 		}
+	}
 
+	return points
+}
+
+func partTwo(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+
+	const (
+		Winning = "Z"
+		Drawing = "Y"
+		Losing  = "X"
+	)
+
+	const (
+		Rock     = "A" // 1 point
+		Paper    = "B" // 2 points
+		Scissors = "C" // 3 points
+	)
+
+	points := 0
+	for scanner.Scan() {
+		strategy := scanner.Text()
+
+		opponentHand := string(strategy[0])
+
+		switch yourHand := string(strategy[2]); yourHand {
+		case Winning:
+			points += 6
+			switch opponentHand {
+			case Rock:
+				points += 2
+			case Paper:
+				points += 3
+			case Scissors:
+				points += 1
+			}
+		case Drawing:
+			points += 3
+			switch opponentHand {
+			case Rock:
+				points += 1
+			case Paper:
+				points += 2
+			case Scissors:
+				points += 3
+			}
+		case Losing:
+			points += 0
+			switch opponentHand {
+			case Rock:
+				points += 3
+			case Paper:
+				points += 1
+			case Scissors:
+				points += 2
+			}
+		}
+	}
+
+	return points
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Part 1 answer
-	fmt.Println("Points:", points)
+	fmt.Println("Points:", partOne(file))
+
+	file, err = os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Part 2 answer
+	fmt.Println("Points:", partTwo(file))
 }
